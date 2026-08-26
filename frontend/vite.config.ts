2976f448f7ef5a74276f52486edc5f071304e091
Sweet Heart A. Torrado <sweetheart.torrado@msugensan.edu.ph>
2026-08-27 01:48:14 +0800

    Improve deployed map boundary visibility

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 2000 },
   optimizeDeps: { exclude: ['maplibre-gl'] },
-  server: { proxy: { '/api': 'http://127.0.0.1:8765', '/files': 'http://127.0.0.1:8765' } },
+  server: {
+    allowedHosts: ['.trycloudflare.com', 'oraclis.jaypee.dpdns.org'],
+    proxy: { '/api': 'http://127.0.0.1:8765', '/files': 'http://127.0.0.1:8765' },
+  },
 })
